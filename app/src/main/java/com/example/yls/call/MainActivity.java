90fc28eed82360edd3c768a053b507ac5c7b940f
MyWeir <836755209@qq.com>
2017-05-11 08:36:17 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/example/yls/call/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.yls.call;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSION_REQUEST_CALL_PHONE = 1;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE},
+                            MY_PERMISSION_REQUEST_CALL_PHONE);
+
+                } else {
+                    callPhone();
+                }
+            }
+
+        });
+    }
+
+
+    public void callPhone() {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        Uri data = Uri.parse("tel:" + "10086");
+        intent.setData(data);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(intent);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permission, int[] gtantResults) {
+        if (requestCode == MY_PERMISSION_REQUEST_CALL_PHONE) {
+            if (gtantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                callPhone();
+            } else {
+                Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+
+            }
+            return;
+        }
+     super.onRequestPermissionsResult(requestCode, permission, gtantResults);
+    }
+}
